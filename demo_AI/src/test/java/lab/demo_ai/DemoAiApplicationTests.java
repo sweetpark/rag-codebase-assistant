f81f192273a74e6a800caae71bb8cda670da3111
sweetpark <wooyeong1998@gmail.com>
2025-07-21 21:45:14 +0900

    Initial

---
+++ demo_AI/src/test/java/lab/demo_ai/DemoAiApplicationTests.java
@@ -0,0 +1,13 @@
+package lab.demo_ai;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoAiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
