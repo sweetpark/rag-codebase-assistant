db2103188a497fd815b01f6adc3ca62688bb278d
sweetpark <wooyeong1998@gmail.com>
2025-08-08 21:57:36 +0900

    [FIX](Controller) MainController.java main controller의 역할에 대해 주석 추가

--- demo_AI/src/main/java/lab/demo_ai/MainController.java
+++ demo_AI/src/main/java/lab/demo_ai/MainController.java
@@ -14,6 +14,18 @@ import org.springframework.web.bind.annotation.RequestBody;
 import java.util.HashMap;
 import java.util.Map;
 
+
+/**
+ * [기능요약] 계정 관련 API 요청을 처리하는 컨트롤러
+ *
+ * [상세설명]
+ * - 프론트에서 전달된 서비스명(`account`)과 작업 유형(`oper`)을 기반으로 요청을 분기 처리
+ * - `AccountService`를 통해 실제 로직을 실행하며 결과를 응답으로 반환
+ *
+ * @param requestBody 요청 파라미터 (service, oper, payload)
+ * @return JSON 형태의 결과 응답
+ */
+
 @Controller
 @Slf4j
 public class MainController {
