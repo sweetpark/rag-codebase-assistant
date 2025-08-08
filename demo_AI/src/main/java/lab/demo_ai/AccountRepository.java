2c1f5d5e9d1ca18056c15df2f39101212c2301e1
sweetpark <wooyeong1998@gmail.com>
2025-08-08 21:56:43 +0900

    [FIX](repository) AccountRepository.java mybatis id값에 맵핑되는 메서드 정리 java파일로 주석 추가

--- demo_AI/src/main/java/lab/demo_ai/AccountRepository.java
+++ demo_AI/src/main/java/lab/demo_ai/AccountRepository.java
@@ -5,6 +5,13 @@ import org.apache.ibatis.annotations.Mapper;
 import java.util.List;
 import java.util.Map;
 
+/**
+ * [기능요약] MyBatis Mapper 호출을 통한 DB 연동 Repository
+ *
+ * [상세설명]
+ * - MyBatis XML 매퍼를 통해 DB의 `member` 테이블을 조작
+ * - 계정 정보 조회, 단건 조회, 등록, 수정, 비밀번호 초기화, ID 중복 체크 기능을 제공
+ */
 @Mapper
 public interface AccountRepository {
     List<Map<String,Object>> sltAccountInfo(Map<String, Object> param);
